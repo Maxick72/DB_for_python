b16229619db39b36e61a573f5fdcddf752dc41ff
Mikhail Petrov <mpetrov71@mail.ru>
2025-04-15 18:45:57 +0500

    full modification homework #2 PNG and drawio

---
+++ SQL_Query_HW2.sql
@@ -0,0 +1,48 @@
+CREATE TABLE musicstyle (
+ styleID serial PRIMARY KEY,
+ name VARCHAR(25) NOT NULL
+ );
+
+CREATE TABLE album (
+ albumID SERIAL PRIMARY KEY,
+ name VARCHAR(100) NOT NULL,
+ release_date DATE NOT NULL
+ );
+ 
+CREATE TABLE artist (
+ artistID SERIAL PRIMARY KEY,
+ name VARCHAR(25) NOT NULL
+ );
+
+CREATE TABLE track (
+ trackID SERIAL PRIMARY KEY,
+ name VARCHAR(100) NOT NULL,
+ duration DECIMAL (3,2) NOT NULL,
+ albumID INTEGER REFERENCES album(albumID) NOT NULL
+ );
+
+CREATE TABLE artist_style (
+ artist_styleID SERIAL PRIMARY KEY NOT null,
+ style_ID INT NOT NULL,
+ artist_ID INT NOT NULL,
+ CONSTRAINT fk_style FOREIGN KEY (style_ID) REFERENCES musicstyle (styleID) ON DELETE CASCADE,
+ CONSTRAINT fk_artist FOREIGN KEY (artist_ID) REFERENCES artist (artistID) ON DELETE CASCADE
+ );
+
+CREATE TABLE album_artist (
+ album_artistID SERIAL PRIMARY KEY,
+ album_ID INT NOT NULL,
+ artist_ID INT NOT NULL,
+ CONSTRAINT fk_album FOREIGN KEY (album_ID) REFERENCES album(albumID) ON DELETE CASCADE,
+ CONSTRAINT fk_artist FOREIGN KEY (artist_ID) REFERENCES artist (artistID) ON DELETE CASCADE
+ ); 
+
+CREATE TABLE collection (
+ collectionID SERIAL PRIMARY KEY NOT NULL,
+ name VARCHAR(100) NOT NULL,
+ collection_date DATE NOT NULL,
+ album_ID INT NOT NULL,
+ track_ID INT NOT NULL,
+ CONSTRAINT fk_album FOREIGN KEY (album_ID) REFERENCES album(albumID) ON DELETE CASCADE,
+ CONSTRAINT fk_track FOREIGN KEY (track_ID) REFERENCES track(trackID) ON DELETE CASCADE
+ ); 
